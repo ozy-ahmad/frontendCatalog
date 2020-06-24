87b3b8be7aee97834b3ce51c3a2eeb9057f1e82f
Ahmad Fakhrozy <ahmadfakhrozy@gmail.com>
2020-06-24 20:16:59 +0700

    add container to navbar component

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -11,48 +11,51 @@ import {
   DropdownToggle,
   DropdownMenu,
   DropdownItem,
+  Container,
 } from "reactstrap";
 
-const Navbars = (props) => {
+const Navbars = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   const toggle = () => setIsOpen(!isOpen);
 
   return (
     <Navbar color="light" light expand="md">
-      <NavbarBrand href="/"> flickr</NavbarBrand>
-      <NavbarToggler onClick={toggle} />
-      <Collapse isOpen={isOpen} navbar>
-        <Nav className="ml-auto" navbar>
-          <NavItem>
-            <Link className="nav-link" to="/mygalery/">
-              {" "}
-              My Galery
-            </Link>
-          </NavItem>
-          <NavItem>
-            <a
-              className="nav-link"
-              href="https://github.com/ozy-ahmad"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              GitHub
-            </a>
-          </NavItem>
-          <UncontrolledDropdown nav inNavbar>
-            <DropdownToggle nav caret>
-              Options
-            </DropdownToggle>
-            <DropdownMenu right>
-              <DropdownItem>Option 1</DropdownItem>
-              <DropdownItem>Option 2</DropdownItem>
-              <DropdownItem divider />
-              <DropdownItem>Reset</DropdownItem>
-            </DropdownMenu>
-          </UncontrolledDropdown>
-        </Nav>
-      </Collapse>
+      <Container>
+        <NavbarBrand href="/"> flickr</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="ml-auto" navbar>
+            <NavItem>
+              <Link className="nav-link" to="/mygalery/">
+                {" "}
+                My Galery
+              </Link>
+            </NavItem>
+            <NavItem>
+              <a
+                className="nav-link"
+                href="https://github.com/ozy-ahmad"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                GitHub
+              </a>
+            </NavItem>
+            <UncontrolledDropdown nav inNavbar>
+              <DropdownToggle nav caret>
+                Options
+              </DropdownToggle>
+              <DropdownMenu right>
+                <DropdownItem>Option 1</DropdownItem>
+                <DropdownItem>Option 2</DropdownItem>
+                <DropdownItem divider />
+                <DropdownItem>Reset</DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown>
+          </Nav>
+        </Collapse>
+      </Container>
     </Navbar>
   );
 };
