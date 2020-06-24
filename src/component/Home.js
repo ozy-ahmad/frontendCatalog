bd5b6480aa215a2946727a5c63652b315231dc3e
Ahmad Fakhrozy <ahmadfakhrozy@gmail.com>
2020-06-24 20:09:45 +0700

    add container to home component

--- src/component/Home.js
+++ src/component/Home.js
@@ -31,34 +31,39 @@ const Home = () => {
     });
 
     return (
-      <Container>
-        <Col key={index} className="mt5">
-          <Card>
-            <CardBody>
-              <CardTitle>
-                <a
-                  href={item.author.uri}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  {" "}
-                  <img src={item.author["flickr:buddyicon"]} />{" "}
-                  {item.author.name}
-                </a>
-              </CardTitle>
-            </CardBody>
-            <a href={alternate.href} target="_blank" rel="noopener noreferrer">
-              {" "}
-              <img width="100%" src={enclosure.href} alt={enclosure.title} />
-            </a>
-            <CardBody>
-              <CardText> {item.title}</CardText>
-            </CardBody>
-          </Card>
-        </Col>
-      </Container>
+      <Col key={index}>
+        <Card className="mt-5">
+          <CardBody>
+            <CardTitle>
+              <a
+                href={item.author.uri}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img
+                  src={item.author["flickr:buddyicon"]}
+                  alt={item.author.name}
+                />
+                {item.author.name}
+              </a>
+            </CardTitle>
+          </CardBody>
+          <a href={alternate.href} target="_blank" rel="noopener noreferrer">
+            {" "}
+            <img width="100%" src={enclosure.href} alt={enclosure.title} />
+          </a>
+          <CardBody>
+            <CardText> {item.title}</CardText>
+          </CardBody>
+        </Card>
+      </Col>
     );
   });
-  return <Row xs="2">{showData}</Row>;
+  return (
+    <Container>
+      {" "}
+      <Row xs="2">{showData}</Row>
+    </Container>
+  );
 };
 export default Home;
